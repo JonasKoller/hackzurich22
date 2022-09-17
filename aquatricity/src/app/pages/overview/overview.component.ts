994c9d209c6dace06fb6a76f93289fbfb0f82b2a
JonasKoller <jonas.koller@gmx.ch>
2022-09-17 11:49:07 +0200

    added current path

--- aquatricity/src/app/pages/overview/overview.component.ts
+++ aquatricity/src/app/pages/overview/overview.component.ts
@@ -8,6 +8,8 @@ import {AuthService, User} from 'src/app/_core/auth.service';
 })
 export class OverviewComponent implements OnInit {
   currentUser: any;
+  finishedPathTasks = ['Boil the right amount of water', 'Switch off devices in standby', 'Always turn off light'];
+  unfinishedPathTasks = ['Cooking with a lid', 'Proper ventilation'];
 
   constructor(private auth: AuthService) { }
 
