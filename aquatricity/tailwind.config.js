3abda905269a4dc8d0bf66dcbd7dd431c987edfb
JonasKoller <jonas.koller@gmx.ch>
2022-09-17 03:06:41 +0200

    add current progress register

--- aquatricity/tailwind.config.js
+++ aquatricity/tailwind.config.js
@@ -3,5 +3,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#22577A",
+          "secondary": "#38A3A5",
+          "accent": "#57CC99",
+          "neutral": "#191D24",
+          "base-100": "#FAFAFA",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
