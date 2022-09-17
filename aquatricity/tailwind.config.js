d5ff7a7c44cb50336031d24177a2a9f607ec485e
davidgataric <daavidgtr@outlook.com>
2022-09-17 03:30:53 +0200

    Added autoprefixer and postcss

--- aquatricity/tailwind.config.js
+++ aquatricity/tailwind.config.js
@@ -7,15 +7,15 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          "primary": "#22577A",
-          "secondary": "#38A3A5",
-          "accent": "#57CC99",
-          "neutral": "#191D24",
+          primary: "#22577A",
+          secondary: "#38A3A5",
+          accent: "#57CC99",
+          neutral: "#191D24",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
           "base-100": "#FAFAFA",
-          "info": "#3ABFF8",
-          "success": "#36D399",
-          "warning": "#FBBD23",
-          "error": "#F87272",
         },
       },
     ],
