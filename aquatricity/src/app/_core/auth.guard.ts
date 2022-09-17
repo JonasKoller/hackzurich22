872a9b979a97f80f9330ca052fe656c6b570174c
davidgataric <daavidgtr@outlook.com>
2022-09-17 13:13:33 +0200

    fix auth guard

--- aquatricity/src/app/_core/auth.guard.ts
+++ aquatricity/src/app/_core/auth.guard.ts
@@ -11,14 +11,14 @@ export class AuthGuard implements CanActivate {
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | boolean {
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.auth.authenticated().then((authenticated) => {
+      if (authenticated !== null) {
+        return true;
+      }
 
-    if (this.auth.authenticated() !== null) {
-      return true;
-    }
-    this.router.navigate(['/login']);
-    return false;
-
-
+      this.router.navigate(['/login']);
+      return false;
+    })
   }
 }
