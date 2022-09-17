410dca8ee5842a8db482375b91b570eb9762121d
Moritz Wicki <wickimoritz09@gmail.com>
2022-09-17 02:15:48 +0200

    add slider-component

--- aquatricity/src/app/app.module.ts
+++ aquatricity/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AppSliderComponent } from './components/app-slider/app-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppSliderComponent,
   ],
   imports: [
     BrowserModule,
---
+++ aquatricity/src/app/components/app-slider/app-slider.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppSliderComponent } from './app-slider.component';
+
+describe('AppSliderComponent', () => {
+  let component: AppSliderComponent;
+  let fixture: ComponentFixture<AppSliderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AppSliderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppSliderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
